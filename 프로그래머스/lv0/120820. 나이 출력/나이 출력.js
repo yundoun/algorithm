752edcf2fc52b3c2dc053b7a09cd07fb230aafbb
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-02-06 13:48:34 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
@@ -1,9 +1,4 @@
 function solution(age) {
-    let AGE = 0;
-    if (age >=22){
-        AGE = 2000-(age-23)
-    }
-    else
-        AGE = 2022 - age +1
-    return  AGE;
+    let AGE = 2022 - age +1
+    return AGE;
 }
