251d4aa99e59322cdc0d9956c38a4bb3745e4ef4
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-02-06 14:12:47 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,7 @@
+function solution(my_string) {
+    var answer = my_string.split('')
+    return (answer.reverse()).join('');
+    
+    // const tmp = tmp.join()  // "h,e,l,l,o"
+    // const tmp2 = tmp.join(+) // "h+e+l+l+o"
+}
