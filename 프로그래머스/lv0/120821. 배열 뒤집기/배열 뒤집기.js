b9533a3864582d6426d24b3b9930a39d146cfaed
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-02-06 14:01:38 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    var reverse = [];
+    let count  = num_list.length
+    
+    for (let i=0; i<count; i++){
+        let tmp = num_list.pop()
+        reverse.push(tmp)
+    }
+    return reverse;
+}
