7a799707a2afa12c9ea53499bc8e4777b96e5872
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-02-06 13:30:00 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -3,8 +3,6 @@ function solution(money) {
     let count, remain = 0;
     count = parseInt(money / 5500);
     remain = money % 5500;
-    let change = [];
-    change.push(count)
-    change.push(remain)
+    let change = [count, remain];
     return change;
 }
