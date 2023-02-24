b63422221eecd37e23944f1932b9b3fb0fc5f3e0
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-02-24 23:51:53 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "|\\_/|" << endl << "|q p|   /}" << endl << "( 0 )\"\"\"\\" << endl
+		<< "\|\"^\"`    \|" << endl << "||_/=\\\\__|";
+}
