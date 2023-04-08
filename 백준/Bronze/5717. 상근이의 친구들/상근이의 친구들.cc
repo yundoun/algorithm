94b3a2f8177e9a02213ba670cd148f9eec11accd
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-04-08 23:03:05 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	while (true) {
+		int a, b = 0;
+		cin >> a >> b;
+
+		
+
+		if (a == 0 && b == 0) {
+			break;
+		}
+
+		cout << a + b << endl;
+
+	}
+}
