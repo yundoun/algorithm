c3065db057912c190ae93e215a03d88a74e54d6b
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-06-24 21:44:22 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -4,7 +4,5 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    (n <= 7) ? answer = 1 : (n%7 == 0) ? answer = n/7 : answer = n/7 + 1;
-    return answer;
+   return  (n <= 7) ? 1 : (n%7 == 0) ? n/7 : n/7 + 1;
 }
