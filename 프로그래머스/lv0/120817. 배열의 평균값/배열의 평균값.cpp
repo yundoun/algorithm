979aca6c6384efe1bd2e4bcfb836993d6289d87e
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-06-24 22:23:00 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    int answer = 0;
+    for (int i =0; i<numbers.size(); i++){
+        answer += numbers[i];
+    }
+    return answer/double(numbers.size());
+}
