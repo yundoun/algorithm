ad2c3f42de6a33aab2bf2eec903801b92e5b5b9c
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-06-26 17:44:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    int a = 0, b =0;
+    for (int i=0; i<num_list.size(); i++){
+        (num_list[i] % 2 == 0 ) ? a++ : b++; 
+    }
+    answer.push_back(a);
+    answer.push_back(b);
+    return answer;
+}
