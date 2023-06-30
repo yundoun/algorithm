9604fd4da523c3f0f49bebfa4d24c17467c35b88
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-06-30 15:51:18 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 3.58 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
@@ -6,15 +6,12 @@ using namespace std;
 int solution(int hp) {
     int answer = 0;
     
-    if (hp/5 >= 1) {
-        answer += hp/5;
-        hp = hp%5;
-    }
-    
-    if (hp/3 >= 1){
-        answer += hp/3;
-        hp = hp%3;
-    }
+    answer += hp/5;
+    hp = hp%5;
+
+    answer += hp/3;
+    hp = hp%3;
+
     answer += hp;
     
     return answer;  
