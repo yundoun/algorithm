60e7d96f07358ee44d47ed5d189b5c8266f0ca7a
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-07-03 16:47:42 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/README.md
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 구슬을 나누는 경우의 수 - 120840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120840#qna) 
 
 ### 성능 요약
 
-메모리: 4.16 MB, 시간: 0.01 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -1,23 +1,15 @@
 #include <string>
 #include <vector>
 
-#define min(a,b) ((a)>(b)?(b):(a))
-#define max(a,b) ((a)>(b)?(a):(b))
-
 using namespace std;
 
 int solution(int balls, int share) {
-    unsigned long long answer = 1;
-    int j = 1;
-    for(int i=min(balls,share)+1;i<=max(balls,share);i++)
-    {
-        if(j > balls-share) answer = answer*i;
-        else
-        {
-            answer = answer*i/j;
-            j++;
-        }
+    unsigned long long result = 1;
+
+    for (int i = 1; i <= share; i++) {
+        result *= balls--;
+        result /= i;
     }
 
-    return answer;
+    return result;
 }
