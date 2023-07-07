738e7aadc3f0b7032229f7df32810e0ba0291b2b
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-07-07 16:32:12 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,22 @@
+#include <queue>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	queue<int>q;
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		q.push(i);
+	}
+
+	while (q.size() != 1) {
+		q.pop();
+		
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front();
+}
