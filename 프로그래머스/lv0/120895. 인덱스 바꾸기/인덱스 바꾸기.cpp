d5156026e7e0494df1d7bf0c95de769cfa7bd81f
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-07-08 21:30:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 
 string solution(string my_string, int num1, int num2) {
-    char tmp;
-    tmp = my_string[num1];
-    my_string.replace(num1, 1, 1, my_string[num2]);
-    my_string.replace(num2, 1, 1, tmp);
+
+    swap(my_string[num1], my_string[num2]);
     
+// 처음 풀이법
+//     char tmp;
+//     tmp = my_string[num1];
+//     my_string.replace(num1, 1, 1, my_string[num2]);
+//     my_string.replace(num2, 1, 1, tmp);
     return my_string;
 }
