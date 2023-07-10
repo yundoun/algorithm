1b2e3593e025b02d67e5f17261cf8f2b8b004b15
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-07-10 15:35:07 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    
+    return (str1.find(str2) == string::npos) ? 2 : 1;
+}
