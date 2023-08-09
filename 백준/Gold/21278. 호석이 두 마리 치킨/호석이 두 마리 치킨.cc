49ccebf33f066a06875692e67f1a1e9c16362991
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-08-09 20:57:10 +0900

    [Gold V] Title: 호석이 두 마리 치킨, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.cc
+++ 백준/Gold/21278. 호석이 두 마리 치킨/호석이 두 마리 치킨.cc
@@ -1,52 +1,79 @@
 #include <iostream>
-#define INF 1e9
-using namespace std;
+#include <algorithm>
+#include <vector>
+#define INF 1000000000;
 
+using	namespace std;
 
 int N, M;
-int d[101][101];
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    
-    for(int i=0; i<101; i++){
-        for(int k=0; k<101; k++){
-            if(i==k) d[i][k] = 0;
-            else d[i][k] = INF;
-        }
-    }
-    cin>>N>>M;
-    
-    for(int i=0; i<M; i++){
-        int a, b; cin>>a>>b;
-        d[a][b] = 1;
-        d[b][a] = 1;
-    }
-    
-    for(int k=1; k<=N; k++){
-        for(int a=1; a<=N; a++){
-            for(int b=1; b<=N; b++){
-                d[a][b] = min(d[a][b], d[a][k] + d[k][b]);
-            }
-        }
-    }
-    
-    int chick1 = 0, chick2 = 0;
-    int min_d = 1e9;
-    
-    for(int i=1; i<=N; i++){    // 치킨집 고르기
-        for(int j=i+1; j<=N; j++){
-            int temp_d = 0;
-            for(int k = 1; k<=N; k++){
-                temp_d += min(i==k?0:d[i][k], j==k?0:d[j][k]);    // 치킨집까지의 거리 갱신
-            }
-            if(temp_d < min_d){
-                min_d = temp_d;
-                chick1 = i;
-                chick2 = j;
-            }
-        }
-    }
-    
-    cout<< chick1 << ' ' << chick2 << ' ' << min_d * 2;
+int ary[101][101];
+int ans_T = INF; // 시간의 최소값
+pair<int, int> ans_P;  // 건물 번호
+
+void input() {
+
+	cin >> N >> M; // 건물의 개수 : N , 도로의 개수 M
+
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			if (i == j) {
+				continue;
+			}
+			ary[i][j] = INF; // 한번에 갈 수 없는 거리는 초기화
+		}
+	}
+
+	for (int i = 0; i < M; i++) {
+		int A, B;
+		cin >> A >> B;
+		ary[A][B] = 1;
+		ary[B][A] = 1;
+	}
+}
+
+void solution() {
+
+	for (int k = 1; k <= N; k++) {
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if (ary[i][j] > ary[i][k] + ary[k][j]) {
+					ary[i][j] = ary[i][k] + ary[k][j];
+				}
+			}
+		}
+	} // 모든 정점에서 모든 정점으로 방문하는 최단 거리를 저장
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = i + 1; j <= N; j++) {
+			int Time_Sum = 0; // 시간의 합
+			for (int k = 1; k <= N; k++) {
+				Time_Sum += min(ary[k][i] * 2, ary[k][j] * 2);
+			}
+			if (ans_T > Time_Sum) {
+				ans_T = Time_Sum; // 최소값 구하기
+				ans_P = make_pair(i, j);
+			}
+		}
+	} // 치킨집을 지을 두 정점의 조합 중 기리의 합이 가장 짧은 조합 구하기
+
+}
+
+void solve() {
+	input();
+	solution();
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+
+	cout << ans_P.first << " " << ans_P.second << " " << ans_T << endl;
+
+	return 0;
+
 }
