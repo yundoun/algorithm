9ff3e4a382c9792d04ed98c502972e48a79ae587
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-08-25 15:05:17 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	char  s;
+
+	cin >> s;
+
+	cout << int(s);
+
+	return 0;
+
+}
+
