7981910202317c1bf2733f9e05f7b1d1859c908a
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-09-19 17:14:25 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 436 ms, Memory: 12508 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .replaceAll("\r", "")
+  .trim()
+  .split("\n");
+
+const n = Number(input[0])
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j < i; j++) {
+    process.stdout.write("*");
+  }
+  console.log("");
+}
+
+
+
+
