47a9c8fe1eedbc35399ebe1c01bf547a67fdf066
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-10-05 16:56:31 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.71 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,19 +1,33 @@
 function solution(n, control) {
     
-    for (let i=0; i<control.length; i++){
-        if (control[i] == "w"){
-            n += 1;
+    // for (let i=0; i<control.length; i++){
+    //     if (control[i] == "w"){
+    //         n += 1;
+    //     }
+    //     else if (control[i] == "s"){
+    //         n -= 1;
+    //     }
+    //     else if (control[i] == "d"){
+    //         n += 10;
+    //     }
+    //     else{
+    //         n -= 10;
+    //     }
+    // }
+    
+    return [...control].reduce((cur, word)=>{
+        if (word === "w"){
+            cur += 1;
         }
-        else if (control[i] == "s"){
-            n -= 1;
+        else if (word === "s"){
+            cur -= 1;
         }
-        else if (control[i] == "d"){
-            n += 10;
+        else if (word === "d"){
+            cur += 10;
         }
         else{
-            n -= 10;
+            cur -= 10;
         }
-    }
-    
-    return n;
+        return cur
+    },n);
 }
