128e89ba3e2721a02e0dcfad0a5417d9a654b052
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-01-10 21:39:51 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2528 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,49 +1,42 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
+#include <string>
 #include <vector>
+#include <queue>
 #include <cstring>
-
-using namespace std;
+using	namespace std;
 
 int N, M, V;
-int a, b;
+int x, y;
+int visit[1001] = { 0, };
+vector<int>adj[10002]; // 인접리스트
+vector<int>bfs; // 방문한 순서
+vector<int>dfs;
+queue<int>q;
 
-vector<int> adj[10002];
-vector<int>ary_dfs;
-vector<int>ary_bfs;
-queue<int> q;
-
-int visit[1001] = {0,};
-
-void dfs(int c) {
-	visit[c] = 1; // 현재 노드 c 방문
-	ary_dfs.push_back(c); // 정답 벡터에 푸시
-	for (int i = 0; i < adj[c].size(); i++) {
-		// 방문한 벡터의 크기만큼 반복
-		if (visit[adj[c][i]] != 1) dfs(adj[c][i]);
-		// 방문하지 않았다면 방문
+void DFS(int s) {
+	visit[s] = 1; 
+	dfs.push_back(s); 
+	for (int i = 0; i < adj[s].size(); i++) {
+		if (visit[adj[s][i]] != 1) DFS(adj[s][i]);
 	}
+
 }
 
+void BFS(int s) {
+	q.push(s);
+	visit[s] = 1;
 
-void bfs(int c) {
-	q.push(c); // 탐색을 진행하기 위해 큐에 푸시
-	visit[c] = 1; // 방문했음 표시
-
-	while (!q.empty()) { 
-		// 큐가 빌떄까지 반복, 큐가 비어있지 않다면 방문하지 않은 노드가 존재한다는 뜻
-
-		int tmp = q.front();  // 탐색할 노드 임시 변수에 저장
-		q.pop(); // 방문한 노드 pop;
-		ary_bfs.push_back(tmp); // 정답 벡터에 푸시
+	while (!q.empty()) {
+		int tmp = q.front();
+		
+		q.pop();
+		bfs.push_back(tmp);
 
 		for (int i = 0; i < adj[tmp].size(); i++) {
-			if (visit[adj[tmp][i]] != 1) { 
-				// 방문하지 않았을 경우
+			if (visit[adj[tmp][i]] != 1) {
 				q.push(adj[tmp][i]);
 				visit[adj[tmp][i]] = 1;
-
 			}
 		}
 	}
@@ -53,9 +46,9 @@ void input() {
 	cin >> N >> M >> V;
 
 	for (int i = 0; i < M; i++) {
-		cin >> a >> b;
-		adj[a].push_back(b);
-		adj[b].push_back(a);
+		cin >> x >> y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
 		// 양방향 간선 처리
 	}
 }
@@ -63,25 +56,24 @@ void input() {
 void solution() {
 
 	for (int i = 1; i <= N; i++) {
-		sort(adj[i].begin(), adj[i].end());  
+		sort(adj[i].begin(), adj[i].end());
 		// 벡터안의 원소 오름차순 정렬
 	}
 
-	dfs(V); // dfs 탐색
+	DFS(V); 
 
 	memset(visit, 0, sizeof(visit));
 
-	bfs(V); // bfs 탐색
+	BFS(V); 
 
-	for (int i = 0; i < ary_dfs.size(); i++) {
-		cout << ary_dfs[i] << " ";
+	for (int i = 0; i < dfs.size(); i++) {
+		cout << dfs[i] << " ";
 	}
 	cout << "\n";
 
-	for (int i = 0; i < ary_bfs.size(); i++) {
-		cout << ary_bfs[i] << " ";
+	for (int i = 0; i <bfs.size(); i++) {
+		cout << bfs[i] << " ";
 	}
-
 }
 
 void solve() {
@@ -100,4 +92,3 @@ int main() {
 	return 0;
 
 }
-
