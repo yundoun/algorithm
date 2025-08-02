d40d528b3601aa132ff922a16e73ef71b138fe41
Doun Yun <106233376+yundoun@users.noreply.github.com>
2025-08-03 00:28:58 +0900

    [level 2] Title: 주식가격, Time: 29.83 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,16 @@
+function solution(prices) {
+    var answer = [];
+    
+    for (let i = 0; i<prices.length; i++){
+        let cnt = 0;
+        for (let j = i+1; j<prices.length; j++){
+            cnt++
+            if (prices[i] > prices[j]) {
+                break;
+            }
+        }
+        answer.push(cnt);
+    }
+    
+    return answer;
+}
