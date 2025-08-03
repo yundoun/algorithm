3707062a3642f866d59cf6e61c235179b1e15774
Doun Yun <106233376+yundoun@users.noreply.github.com>
2025-08-03 17:45:31 +0900

    [level 2] Title: 주식가격, Time: 30.41 ms, Memory: 41.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -3,12 +3,16 @@ function solution(prices) {
     
     for (let i = 0; i<prices.length; i++){
         let cnt = 0;
+        
+        // 순회하는 가격의 다음 가격부터 체크
         for (let j = i+1; j<prices.length; j++){
             cnt++
+            // 가격이 떨어졌다면 카운트 종료
             if (prices[i] > prices[j]) {
                 break;
             }
         }
+        // 카운트 한 것 정답 배열에 푸시
         answer.push(cnt);
     }
     
