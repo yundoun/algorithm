54b113ddb2342ed33921cc60248a08c740e66bcd
Doun Yun <106233376+yundoun@users.noreply.github.com>
2025-08-03 17:46:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 38.37 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -3,6 +3,7 @@ function solution(arr)
     var answer = [];
 
     for (let i = 0; i<arr.length; i++){
+        // 순회하는 기준 요소와 그다음 요소가 다를 경우에만 정답 배열에 푸시
         if(arr[i] !== arr[i+1]) answer.push(arr[i])
     }
     
