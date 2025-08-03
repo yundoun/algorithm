2aa3cf63b91fcb8c5f492e537600fa195b0e76cd
Doun Yun <106233376+yundoun@users.noreply.github.com>
2025-08-03 16:06:09 +0900

    [level 2] Title: 기능개발, Time: 3.60 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,38 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    let remain = [];
+    // 1. progresses과 speeds를 순회하면서 remain이라는 배열에 남은 기간을 저장한다.
+    // 2. 반복문에 remain을 순회한다
+    // 3. 첫번째 인덱스부터 어떤 변수에 값을 저장하면서 해당 변수보다 큰 값이 나올때까지 순회한다. 이때 cnt로 배포 날짜를 계산한다.
+    // 4. 큰 값이 나오면 cnt의 값을 answer에 push한다.
+    
+    for (let i =0; i<progresses.length; i++){
+        // 남은 작업 값
+        let remainValue = (100-progresses[i])
+        // 걸리는 시간, ceil는 소수점이 있을 경우 무조건 올림
+        let workTime = Math.ceil(remainValue/speeds[i])
+        remain.push(workTime)
+    }
+    
+    console.log(remain)
+    
+    for (let i = 0; i<remain.length; i++){
+        let j = 0
+        let cnt = 1;
+        let max = remain[i];
+        for (j = i+1; j<remain.length; j++){
+            // 앞선 작업보다 시간이 적게 걸리면 카운트
+            if (max >= remain[j]){
+                cnt++
+            } 
+            // 많이 걸리면 배포하고 다시 계산
+            else{
+                break
+            }
+        }
+        answer.push(cnt)
+        i = j-1
+    }
+    
+    return answer;
+}
