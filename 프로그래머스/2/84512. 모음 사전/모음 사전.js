37e9e85a2d0f086cfb7a667a04e458da8ac667d3
Doun Yun <106233376+yundoun@users.noreply.github.com>
2025-08-19 21:35:50 +0900

    [level 2] Title: 모음 사전, Time: 0.91 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,17 +1,29 @@
 function solution(word) {
-  let vowels = ["A","E","I","O","U"];
-  let dict = [];
-  
-  function dfs(curr) {
-    if (curr.length > 5) return;
-    if (curr.length > 0) dict.push(curr); // 단어 등록
-    
-    for (let v of vowels) {
-      dfs(curr + v);
+    var count = 0;
+    let 모음 = ["A", "E", "I", "O", "U"];
+
+    for (let a of 모음){
+        count++
+        if (a === word) return count;
+        for (let b of 모음){
+            let w2 = a + b
+            count++
+            if (w2 === word) return count;
+            for (let c of 모음){
+                let w3 = w2 + c;
+                count++
+                if (w3 === word) return count;
+                for (let d of 모음){
+                    let w4 = w3 + d;
+                    count++
+                    if (w4 === word) return count;
+                    for (let e of 모음){
+                        let w5 = w4 + e;
+                        count++
+                        if (w5 === word) return count;
+                    }
+                }
+            }
+        }
     }
-  }
-  
-  dfs("");
-  
-  return dict.indexOf(word) + 1; // index는 0부터니까 +1
 }
